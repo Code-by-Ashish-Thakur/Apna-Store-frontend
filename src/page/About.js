66a80602793cfdecbe5a9ce9540a5a2d523087d8
Ashish <1ashishthakur2002@gmail.com>
2023-10-01 23:01:47 +0530

    update

---
+++ src/page/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <h3>Created By Ashish Thakur </h3>
+      <p>
+        In This site all type of item will be available..
+      </p>
+    </>
+  )
+}
+
+export default About
